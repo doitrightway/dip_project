64c13a5c238b14ec2cc0a25bff59c0a58f2db0d8
Kumar Saurav <mailkr.srv@gmail.com>
2018-11-22 15:47:27 +0530

    func

--- cus.py
+++ cus.py
@@ -1,6 +1,6 @@
 from keras import backend as K
 from keras.engine.topology import Layer
-
+import tensorflow_probability as tfp
 
 class MyLayer(Layer):
 
@@ -54,28 +54,46 @@ class MyLayerDense(Layer):
 
 class MyLayerRelu(Layer):
 
-    def __init__(self, filter_shape, num_layers, **kwargs):
-        self.filter_shape= filter_shape
-        self.num_layers= num_layers
+    def __init__(self, **kwargs):
         super(MyLayerRelu, self).__init__(**kwargs)
 
     def build(self, input_shape):
         # Create a trainable weight variable for this layer.
-        y= int(input_shape[3]/2)
-        self.kernel = self.add_weight(name='kernel', 
-                                      shape=(self.filter_shape,
-                                        self.filter_shape, y,self.num_layers),
-                                      initializer='uniform',
-                                      trainable=True)
         super(MyLayerRelu, self).build(input_shape)  # Be sure to call this at the end
 
     def call(self, x):
         y=int(K.int_shape(x)[3]/2)
-        return K.dot()
-        return K.concatenate([K.conv2d(x[:,:,:,0:y],self.kernel),
-          K.conv2d(x[:,:,:,y:2*y],self.kernel)])
+        z= Lambda(lambda inputs: inputs[0]/inputs[1] if inputs[1]!=0 else 1000000)([x[:,:,:,0:y],x[:,:,:,y:2*y]])
+        tfd = tfp.distributions
+        dist = tfd.Normal(loc=0., scale=1.)
+        var1 = dist.cdf(z)
+        var2 = dist.prob(z)
+        mean = K.dot(x[:,:,:,0:y],var1) + K.dot(x[:,:,:,y:2*y],var2)
+        var3 = K.dot(x[:,:,:,0:y]+x[:,:,:,y:2*y],var1) 
+        var4 = K.dot(K.dot(x[:,:,:,0:y],x[:,:,:,y:2*y]),var2)
+        variance = var3+var4-K.square(mean)
+        return K.concatenate([mean,variance])
 
     def compute_output_shape(self, input_shape):
-        return (input_shape[0], input_shape[1]-self.filter_shape+1,
-          input_shape[1]-self.filter_shape+1,2*self.num_layers)
+        return input_shape
+
+
+class MyFlatten(Layer):
+
+    def __init__(self, **kwargs):
+        super(MyLayerRelu, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        # Create a trainable weight variable for this layer.
+        super(MyLayerRelu, self).build(input_shape)  # Be sure to call this at the end
+
+    def call(self, x):
+        y=int(K.int_shape(x)[3]/2)
+        return K.concatenate([K.batch_flatten(x[:,:,:,0:y]), K.batch_flatten(x[:,:,:,y:2*y])])
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0],input_shape[1]*input_shape[2]*input_shape[3])
+
+
+
 
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from keras.models import Sequential
 from keras.layers import Dense
 from cus import MyLayer
+from cus import MyLayerDense
+from cus import MyLayerRelu
 from keras.datasets import mnist
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
@@ -24,11 +26,19 @@ epochs=40
 #   plt.yticks([])
 # plt.show()
 
+x_train=x_train/255;
+y_train=y_train/255;
+
 x_train = x_train.reshape((np.shape(x_train)+(1,)))
 x_test = x_test.reshape((np.shape(x_test)+(1,)))
 
-x_train=x_train/255;
-y_train=y_train/255;
+noise_train = 0.01*np.random.randn(np.shape(x_train))
+x_train= np.concatenate([x_train,noise_train],axis=-1)
+
+noise_test = 0.01*np.random.randn(np.shape(x_test))
+x_test= np.concatenate([x_test,noise_test])
+
+
 
 print(np.shape(x_train))
 
@@ -39,37 +49,43 @@ print(np.shape(y_train))
 
 model = Sequential()
 
+model.add(MyLayer(filter_shape=3,num_layers=32))
+model.add(MyLayerRelu())
+model.add(MyLayer(filter_shape= 3,num_layers=64)
+model.add(MyLayerRelu())
+model.add()
+
 # model.add(MyLayer(filter_shape=3,num_layers=5))
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=(28,28,1)))
-#32 convolution filters used each of size 3x3
-#again
-model.add(Conv2D(64, (3, 3), activation='relu'))
-#64 convolution filters used each of size 3x3
-#choose the best features via pooling
-model.add(MaxPooling2D(pool_size=(2, 2)))	
-#randomly turn neurons on and off to improve convergence
-model.add(Dropout(0.25))
-#flatten since too many dimensions, we only want a classification output
-model.add(Flatten())
-#fully connected to get all relevant data
-model.add(Dense(128, activation='relu'))
-#one more dropout for convergence' sake :) 
-model.add(Dropout(0.5))
-#output a softmax to squash the matrix into output probabilities
-model.add(Dense(10, activation='softmax'))
+# model.add(Conv2D(32, kernel_size=(3, 3),
+#                  activation='relu',
+#                  input_shape=(28,28,1)))
+# #32 convolution filters used each of size 3x3
+# #again
+# model.add(Conv2D(64, (3, 3), activation='relu'))
+# #64 convolution filters used each of size 3x3
+# #choose the best features via pooling
+# model.add(MaxPooling2D(pool_size=(2, 2)))	
+# #randomly turn neurons on and off to improve convergence
+# model.add(Dropout(0.25))
+# #flatten since too many dimensions, we only want a classification output
+# model.add(Flatten())
+# #fully connected to get all relevant data
+# model.add(Dense(128, activation='relu'))
+# #one more dropout for convergence' sake :) 
+# model.add(Dropout(0.5))
+# #output a softmax to squash the matrix into output probabilities
+# model.add(Dense(10, activation='softmax'))
 
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test))
+# sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+# model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+# model.fit(x_train, y_train,
+#           batch_size=batch_size,
+#           epochs=epochs,
+#           verbose=1,
+#           validation_data=(x_test, y_test))
 
 
-model.compile(loss='categorical_crossentropy',
-              optimizer='sgd',
-              metrics=['accuracy'])
+# model.compile(loss='categorical_crossentropy',
+#               optimizer='sgd',
+#               metrics=['accuracy'])
 
